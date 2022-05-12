bb5c60eda4c024f5230d4f8bbb3640df48473ca1
Zaikin Valentin <zaikinvala59@gmail.com>
2022-05-13 00:56:58 +0300

    add jwt auth

---
+++ src/main/java/com/coursework/Kotik/Controllers/CartController.java
@@ -0,0 +1,61 @@
+//package com.coursework.Kotik.Controllers;
+//
+//import com.coursework.Kotik.Models.Purchase;
+//import com.coursework.Kotik.Models.User;
+//import com.coursework.Kotik.Service.CustomUserDetailsService;
+//import com.coursework.Kotik.Service.ProductService;
+//import com.coursework.Kotik.Service.PurchaseService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@Controller
+//public class CartController {
+//    @Autowired
+//    private CustomUserDetailsService userDetailsService;
+//
+//    @Autowired
+//    private PurchaseService purchaseService;
+//
+//    @Autowired
+//    private ProductService productService;
+//
+//    @GetMapping("/cart")
+//    public String home(Authentication authentication, Model model) {
+//        User user = ((User) userDetailsService.findByUsername(authentication.getName()));
+//        int sum = 0;
+//        List<Purchase> purchaseList = purchaseService.getAllPurchaseUser(user.getId());
+//        for (Purchase purchase: purchaseList){
+//            sum += productService.findProductById(purchase.getProductId()).getPrice() * purchase.getProduct_count();
+//        }
+//        model.addAttribute("total", sum);
+//        model.addAttribute("purchaseList", purchaseList);
+//        model.addAttribute("productService", productService);
+//        return "cart";
+//    }
+//
+//    @ResponseBody
+//    @PostMapping("/cart/addPurchase/{id}")
+//    public void addProduct(Authentication authentication, @PathVariable("id") Long productId, Model model){
+//        User user = userDetailsService.findByUsername(authentication.getName());
+//        System.out.println(user.getUsername());
+//
+//        if(authentication.isAuthenticated()){
+//            Purchase purchase = purchaseService.getPurchaseByUserIdAndProductId(user.getId(), productId);
+//            if (purchase == null){
+//                Purchase newPurchase = new Purchase();
+//                newPurchase.setUserId(user.getId());
+//                newPurchase.setProductId(productId);
+//                newPurchase.setProduct_count(1);
+//                purchaseService.addPurchase(newPurchase);
+//            } else {
+//                purchase.setProduct_count(purchase.getProduct_count() + 1);
+//                purchaseService.addPurchase(purchase);
+//            }
+//        }
+//    }
+//}
---
+++ src/main/java/com/coursework/Kotik/Controllers/ProductController.java
@@ -0,0 +1,23 @@
+package com.coursework.Kotik.Controllers;
+
+import com.coursework.Kotik.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProductController {
+    @Autowired
+    private ProductService mainService;
+
+    @GetMapping("/{type}")
+    public String cats(Model model, @PathVariable("type") String type) {
+        model.addAttribute("products", mainService.findByType(type));
+        model.addAttribute("type", "Everything for " + type);
+        return "product";
+    }
+
+}
+
